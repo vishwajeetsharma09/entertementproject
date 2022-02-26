3263d615795d41334d94cf11ead614560a6bdc84
2000031822-VISHWAJEET SHARMA <2000031822@kluniversity.in>
2022-02-26 23:13:25 +0530

    hello

--- website/static/index.js
+++
@@ -1,8 +0,0 @@
-function deletefeedback(feedbackId) {
-  fetch("/delete-feedback", {
-    method: "POST",
-    body: JSON.stringify({ feedbackId: feedbackId }),
-  }).then((_res) => {
-    window.location.href = "/feedback";
-  });
-}
